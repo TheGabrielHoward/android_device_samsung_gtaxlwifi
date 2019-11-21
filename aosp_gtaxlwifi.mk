3c9694c1e3b9bcea2d0304db17ae01863672e80e
followmsi <followmsi@gmail.com>
2019-11-21 15:25:29 +0100

    Pixel-fy

--- aosp_gtaxlwifi.mk
+++ aosp_gtaxlwifi.mk
@@ -21,11 +21,11 @@ $(call inherit-product, device/samsung/gtaxlwifi/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Inherit some common PE stuff.
+$(call inherit-product, vendor/aosp/config/common_full_tablet_wifionly.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_gtaxlwifi
+PRODUCT_NAME := aosp_gtaxlwifi
 PRODUCT_DEVICE := gtaxlwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T580
@@ -40,5 +40,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys
+
+# PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 TARGET_VENDOR := Samsung
